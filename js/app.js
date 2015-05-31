c966ad9cb4f0cf4e6be1e4770ab0cf520f79c7c1
Kathy Dieser <katdiese@gmail.com>
2015-05-30 20:48:57 -0600

    added traverse array on click function to change questions and fun facts on click

---
+++ js/app.js
@@ -0,0 +1,42 @@
+$(document).ready(function() {
+  console.log("ready!");
+  
+  var question = [
+    "What is the smallest country in the world?",
+    "Which country is the Durian Fruit native to?",
+    'Which country has a municipality called "Saint-Louis-Du-Ha!-Ha!"',
+    "What is the newest country in the world?",
+    "What country has the smallest population in the world?"
+  ];
+//  var questionLength = question.length;
+  
+  var funFact = [
+    "",
+    "The Vatican is the smallest country in the world, it fits within the Italian Capital of Rome and is the center of the Catholic Church!  It is also known as the Holy See.",
+    "The Durian Fruit is native to Thailand, and has a small that has been described as some to be similar to rotten onions, turpentine, and raw sewege. It has been banned from certain hotels and public transportation (including airports) in Southeast Asia",
+    "Saint-Louis-du-Ha! Ha! is the only town in the world with two exclamation markets in its name. Ha-ha is an archaic French word for an unexpected obstacle or abruptly ending path.",
+    "South Sudan gained independence from Sudan in July 2011 as the outcome of a 2005 peace deal that ended Africa's longest running civil war.",
+    "The population of the Pitcairn Islands has fluctuated between 40 and 60 people in recent years."
+  ];
+//  var funFactLength = funFact.length;
+  var counter = 0;
+//  console.log(funFact[funFactCounter]);
+  
+  $(".submit").click(function() {
+      counter++;
+      if(counter < 5){
+        $(".fact").html("Fun Fact!<br>" + funFact[counter]);
+        $(".topQuestion").html(question[counter]);
+      }
+      else if (counter >= 5) {
+        $(".finish").show();
+        $(".form").hide();
+      }
+    });
+  
+  $(".tryAgain").click(function(e) {
+    location.reload(false);
+  });
+    
+  
+});
