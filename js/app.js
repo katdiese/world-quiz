bfc3015067e31f5bb5bd9f79b2ddf2c7f8c3b387
Kathy Dieser <katdiese@gmail.com>
2015-06-05 13:21:45 -0600

    changed from looking at array index values

--- js/app.js
+++ js/app.js
@@ -7,57 +7,60 @@ $(document).ready(function() {
   var questions = [{
     question: "What is the smallest country in the world?",
     choice: ["The Vatican", "Zimbabwe", "The Philippine Islands","Nepal","Costa Rica"],
-    correct: 0 },
+    correct: 0,
+    funFact: "The Vatican fits within the Italian Capital of Rome and is the center of the Catholic Church! It is also known as the Holy See."},
                   {
     question: "Which country is the Durian Fruit native to?", 
     choice:  ["Australia", "Russia", "Thailand", "Brazil", "Greenland"],
-    correct: 2},
+    correct: 2,
+    funFact: "The Durian Fruit is native to Thailand and has a smell that has been described by some to be similar to rotten onions, turpentine, and raw sewege. It has been banned from certain hotels and public transportation (including airports) in Southeast Asia"},
                   {
     question: 'Which country has a municipality called "Saint-Louis-Du-Ha!-Ha!"',                
     choice: ["Germany", "France", "New Zealand", "Switzerland", "Canada"],
-    correct: 4 },
+    correct: 4 ,
+    funFact:"Saint-Louis-du-Ha! Ha! is in Quebec, Canada, and is the only town in the world with two exclamation marks in its name. Ha-ha is an archaic French word for an unexpected obstacle or abruptly ending path. "             },
                   {
     question: "What is the newest country in the world?",            
     choice: ["United States", "South Sudan", "Scotland", "Montenegro", "Quebec"],
-    correct: 1 },
+    correct: 1,
+    funFact: "South Sudan gained independence from Sudan in July 2011 as the outcome of a 2005 peace deal that ended Africa's longest running civil war."},
                    {
     question: "What country has the smallest population in the world?",                 
     choice: ["The Vatican", "Antigua", "The Pitcairn Islands", "Liechtenstein", "Monaco"],
-    correct: 2 }]
-  
-  var funFact = [
-    "",
-    "The Vatican fits within the Italian Capital of Rome and is the center of the Catholic Church! It is also known as the Holy See.",
-    "The Durian Fruit is native to Thailand and has a smell that has been described by some to be similar to rotten onions, turpentine, and raw sewege. It has been banned from certain hotels and public transportation (including airports) in Southeast Asia",
-    "Saint-Louis-du-Ha! Ha! is in Quebec, Canada, and is the only town in the world with two exclamation marks in its name. Ha-ha is an archaic French word for an unexpected obstacle or abruptly ending path. ",
-    "South Sudan gained independence from Sudan in July 2011 as the outcome of a 2005 peace deal that ended Africa's longest running civil war.",
-    "The population of the Pitcairn Islands has fluctuated between 40 and 60 people in recent years."
-  ]
+    correct: 2,
+    funFact: "The population of the Pitcairn Islands has fluctuated between 40 and 60 people in recent years."              }]
+  var counter = 0;
+  var currentQuestion = questions[counter];
+  nextQuestion(currentQuestion);
    
-   $(".content").on("click", "#enter",function() {
+   $(".content").on("click", "#enter",function(e) {
+     e.preventDefault();
      score();
-     currentQuestion++;
-     nextQuestion();
+     var funFact = currentQuestion.funFact;
+     counter++;
+     currentQuestion = questions[counter];
+     nextQuestion(currentQuestion);
      updateProgress();
-     $(".fact").html("Fun Fact!<br>" + funFact[currentQuestion]);
+      $(".fact").html("Fun Fact!<br>" + funFact);
    });
    
-  function nextQuestion(){
-    if(currentQuestion < 5) {
+  function nextQuestion(myQuestion){
+    if(counter < 5) {
       $(".topQuestion").remove();
       $(".choiceBoxes").remove();
       var newQuestion = '<h2 class="topQuestion">'
-      + questions[currentQuestion].question + '</h2>'
+      + myQuestion.question + '</h2>'
       +'<div class="choiceBoxes"><input type="radio" name="guess" class="guess" value="0">'
-      +questions[currentQuestion].choice[0]   
+      +myQuestion.choice[0]   
       +'<br><input type="radio" name="guess" class="guess" value="1">'
-      +questions[currentQuestion].choice[1]
+      +myQuestion.choice[1]
       +'<br><input type="radio" name="guess" class="guess" value="2">'
-      +questions[currentQuestion].choice[2]
+      +myQuestion.choice[2]
       +'<br><input type="radio" name="guess" class="guess" value="3">'
-      +questions[currentQuestion].choice[3] 
+      +myQuestion.choice[3] 
       +'<br><input type="radio" name="guess" class="guess" value="4">'
-      +questions[currentQuestion].choice[4] + '</div>';
+      +myQuestion.choice[4] + '</div>';
+     
       }
     else {
       $(".finish").show(); 
@@ -71,7 +74,7 @@ $(document).ready(function() {
    
   function score() {
     var answer=$("input[type='radio']:checked").val();
-    if(answer == questions[currentQuestion].correct) {
+    if(answer == currentQuestion.correct) {
         console.log('hooray!');
         currentScore++;
       }
